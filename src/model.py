b8ee79eb39131ef179bc0466dda61051b0e1c68a
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-12-17 15:00:58 +0700

    update requirement and promt

--- src/model.py
+++ src/model.py
@@ -20,16 +20,16 @@ class Model(nn.Module):
         self.clip, _ = clip.load('ViT-B/32', device=device)
         self.clip.apply(freeze_all_but_bn)
         
-        self.sk_prompt = nn.Parameter(torch.randn(self.opts.n_prompts, self.opts.prompt_dim))
-        self.img_prompt = nn.Parameter(torch.randn(self.opts.n_prompts, self.opts.prompt_dim))
+        self.sk_prompt = nn.Parameter(torch.randn(self.opts.n_prompts, self.opts.prompt_dim)).to(device)
+        self.img_prompt = nn.Parameter(torch.randn(self.opts.n_prompts, self.opts.prompt_dim)).to(device)
         
         self.distance_fn = lambda x, y: 1.0 - F.cosine_similarity(x, y)
         
     def forward(self, data, dtype='image'):
         if dtype == 'image':
             feat = self.clip.encode_image(
-                data, self.img_prompt.expand(data.shape[0], -1, -1))
+                data, self.img_prompt.expand(data.shape[0], -1, -1)).to(device)
         else:
             feat = self.clip.encode_image(
-                data, self.sk_prompt.expand(data.shape[0], -1, -1))
+                data, self.sk_prompt.expand(data.shape[0], -1, -1)).to(device)
         return feat
