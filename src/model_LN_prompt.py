d62101dc45449f8237526f4ae4a008e82c43ad1b
tuan.nh <tuan.nh@tinasoft.com.vn>
2026-01-01 20:31:23 +0700

    update prompts from single to couple

--- src/model_LN_prompt.py
+++ src/model_LN_prompt.py
@@ -101,7 +101,7 @@ class Model(pl.LightningModule):
         
         for idx, sk_feat in enumerate(query_feat_all):
             category = all_category[idx]
-            distance = -1*self.distance_fn(sk_feat.unsqueeze(0), gallery)
+            distance = 1*self.distance_fn(sk_feat.unsqueeze(0), gallery)
             top_k_actual = min(top_k, len(gallery)) 
             
             # target = torch.zeros(len(gallery), dtype=torch.bool, device=device)
